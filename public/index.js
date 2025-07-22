a26637d004c1bc873939dc93edc2e80ea1913754
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-07-22 20:26:00 +0500

    Added onopen and onclose

--- public/index.js
+++ public/index.js
@@ -1,15 +1,30 @@
 const eventSource = new EventSource("/api/live-price");
 
 const livePriceElement = document.getElementById("price-display");
+const connectionStatus = document.getElementById('connection-status');
 
 // Handle live price updates
 eventSource.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    const price = data.price;
-    livePriceElement.textContent = price; 
+    try {
+        const data = JSON.parse(event.data);
+        if (data.event === 'price-update') {
+            const price = data.price;
+            livePriceElement.textContent = price;
+        }
+    }
+    catch (err) { 
+        console.error('Invalid data format', err);
+    }
 }
 
+eventSource.onopen = () => {
+    connectionStatus.textContent = 'Live Prices 🟢';
+}
+
+
+
 // Handle connection loss
-eventSource.onerror = () =>{
+eventSource.onerror = () => {
+    connectionStatus.textContent = "Offline 🔴";
     console.error("Connection lost. Attempting to reconnect...");
 }
